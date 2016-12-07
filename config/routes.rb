7ce8ec83c5ef2a1211942fafbce8ce3e11b750a1
gbennum <george.bennum@gmail.com>
2016-12-06 21:56:13 -0500

    Pictures on suite rooms, main page shenanigans

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'welcome/109main'
   get 'suites/pinnacle'
   get 'suites/sterling'
+  get 'welcome/stowe'
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
